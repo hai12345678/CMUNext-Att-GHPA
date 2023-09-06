27bcec417659997a4aeb6db42c9856a8e24672a6
hai12345678 <104504128+hai12345678@users.noreply.github.com>
2023-09-06 21:05:06 +0800

    Update and rename cmu-att-GHPA to cmu-att-GHPA.py

---
+++ cmu-att-GHPA.py
@@ -0,0 +1,319 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x):
+        return self.fn(x) + x
+
+
+class CMUNeXtBlock(nn.Module):
+    def __init__(self, ch_in, ch_out, depth=1, k=3):
+        super(CMUNeXtBlock, self).__init__()
+        self.block = nn.Sequential(
+            *[nn.Sequential(
+                Residual(nn.Sequential(
+                    # deep wise
+                    nn.Conv2d(ch_in, ch_in, kernel_size=(k, k), groups=ch_in, padding=(k // 2, k // 2)),
+                    nn.GELU(),
+                    nn.BatchNorm2d(ch_in)
+                )),
+                nn.Conv2d(ch_in, ch_in * 4, kernel_size=(1, 1)),
+                nn.GELU(),
+                nn.BatchNorm2d(ch_in * 4),
+                nn.Conv2d(ch_in * 4, ch_in, kernel_size=(1, 1)),
+                nn.GELU(),
+                nn.BatchNorm2d(ch_in)
+            ) for i in range(depth)]
+        )
+        self.up = conv_block(ch_in, ch_out)
+
+    def forward(self, x):
+        x = self.block(x)
+        x = self.up(x)
+        return x
+
+
+class conv_block(nn.Module):
+    def __init__(self, ch_in, ch_out):
+        super(conv_block, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(ch_in, ch_out, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(ch_out, ch_out, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class up_conv(nn.Module):
+    def __init__(self, ch_in, ch_out):
+        super(up_conv, self).__init__()
+        self.up = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Conv2d(ch_in, ch_out, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.up(x)
+        return x
+
+
+class fusion_conv(nn.Module):
+    def __init__(self, ch_in, ch_out):
+        super(fusion_conv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(ch_in, ch_in, kernel_size=3, stride=1, padding=1, groups=2, bias=True),
+            nn.GELU(),
+            nn.BatchNorm2d(ch_in),
+            nn.Conv2d(ch_in, ch_out * 4, kernel_size=(1, 1)),
+            nn.GELU(),
+            nn.BatchNorm2d(ch_out * 4),
+            nn.Conv2d(ch_out * 4, ch_out, kernel_size=(1, 1)),
+            nn.GELU(),
+            nn.BatchNorm2d(ch_out)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class Attention_block(nn.Module):
+    def __init__(self, F_g, F_l, F_int):
+        super(Attention_block, self).__init__()
+        # print("F_g:", F_g)
+        # print("F_l:", F_l)
+        # print("F_int:", F_int)
+        self.W_g = nn.Sequential(
+            nn.Conv2d(F_g, F_int, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_l, F_int, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+
+        self.psi = nn.Sequential(
+            nn.Conv2d(F_int, 1, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid()
+        )
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, g, x):
+        # print("Shape of g:", g.shape)
+        # print("Shape of x:", x.shape)
+        # 下采样的gating signal 卷积
+        g1 = self.W_g(g)
+        # 上采样的 l 卷积
+        x1 = self.W_x(x)
+        # print("Shape of g1:", g1.shape)
+        # print("Shape of x1:", x1.shape)
+        # concat + relu
+        psi = self.relu(g1 + x1)
+        # channel 减为1，并Sigmoid,得到权重矩阵
+        psi = self.psi(psi)
+        # 返回加权的 x
+        return x * psi
+
+
+class LayerNorm(nn.Module):
+    r""" From ConvNeXt (https://arxiv.org/pdf/2201.03545.pdf)
+    """
+
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError
+        self.normalized_shape = (normalized_shape,)
+
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
+
+
+
+class Grouped_multi_axis_Hadamard_Product_Attention(nn.Module):
+    def __init__(self, ch_in, ch_out, x=8, y=8):
+        super().__init__()
+
+        c_dim_in = ch_in // 4
+        k_size = 3
+        pad = (k_size - 1) // 2
+
+        self.params_xy = nn.Parameter(torch.Tensor(1, c_dim_in, x, y), requires_grad=True)
+        nn.init.ones_(self.params_xy)
+        self.conv_xy = nn.Sequential(nn.Conv2d(c_dim_in, c_dim_in, kernel_size=k_size, padding=pad, groups=c_dim_in),
+                                     nn.GELU(), nn.Conv2d(c_dim_in, c_dim_in, 1))
+
+        self.params_zx = nn.Parameter(torch.Tensor(1, 1, c_dim_in, x), requires_grad=True)
+        nn.init.ones_(self.params_zx)
+        self.conv_zx = nn.Sequential(nn.Conv1d(c_dim_in, c_dim_in, kernel_size=k_size, padding=pad, groups=c_dim_in),
+                                     nn.GELU(), nn.Conv1d(c_dim_in, c_dim_in, 1))
+
+        self.params_zy = nn.Parameter(torch.Tensor(1, 1, c_dim_in, y), requires_grad=True)
+        nn.init.ones_(self.params_zy)
+        self.conv_zy = nn.Sequential(nn.Conv1d(c_dim_in, c_dim_in, kernel_size=k_size, padding=pad, groups=c_dim_in),
+                                     nn.GELU(), nn.Conv1d(c_dim_in, c_dim_in, 1))
+
+        self.dw = nn.Sequential(
+            nn.Conv2d(c_dim_in, c_dim_in, 1),
+            nn.GELU(),
+            nn.Conv2d(c_dim_in, c_dim_in, kernel_size=3, padding=1, groups=c_dim_in)
+        )
+
+        self.norm1 = LayerNorm(ch_in, eps=1e-6, data_format='channels_first')
+        self.norm2 = LayerNorm(ch_in, eps=1e-6, data_format='channels_first')
+
+        self.ldw = nn.Sequential(
+            nn.Conv2d(ch_in, ch_in, kernel_size=3, padding=1, groups=ch_in),
+            nn.GELU(),
+            nn.Conv2d(ch_in, ch_out, 1),
+        )
+
+    def forward(self, x):
+        x = self.norm1(x)
+        x1, x2, x3, x4 = torch.chunk(x, 4, dim=1)
+        B, C, H, W = x1.size()
+        # ----------xy----------#
+        params_xy = self.params_xy
+        x1 = x1 * self.conv_xy(F.interpolate(params_xy, size=x1.shape[2:4], mode='bilinear', align_corners=True))
+        # ----------zx----------#
+        x2 = x2.permute(0, 3, 1, 2)
+        params_zx = self.params_zx
+        x2 = x2 * self.conv_zx(
+            F.interpolate(params_zx, size=x2.shape[2:4], mode='bilinear', align_corners=True).squeeze(0)).unsqueeze(0)
+        x2 = x2.permute(0, 2, 3, 1)
+        # ----------zy----------#
+        x3 = x3.permute(0, 2, 1, 3)
+        params_zy = self.params_zy
+        x3 = x3 * self.conv_zy(
+            F.interpolate(params_zy, size=x3.shape[2:4], mode='bilinear', align_corners=True).squeeze(0)).unsqueeze(0)
+        x3 = x3.permute(0, 2, 1, 3)
+        # ----------dw----------#
+        x4 = self.dw(x4)
+        # ----------concat----------#
+        x = torch.cat([x1, x2, x3, x4], dim=1)
+        # ----------ldw----------#
+        x = self.norm2(x)
+        x = self.ldw(x)
+        return x
+
+
+
+class CMUNeXt(nn.Module):
+    def __init__(self, input_channel=3, num_classes=1, dims=[16, 32, 128, 160, 256], depths=[1, 1, 1, 3, 1], kernels=[3, 3, 7, 7, 7]):
+        """
+        Args:
+            input_channel : input channel.
+            num_classes: output channel.
+            dims: length of channels
+            depths: length of cmunext blocks
+            kernels: kernal size of cmunext blocks
+        """
+        super(CMUNeXt, self).__init__()
+        # Encoder
+        self.Maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.stem = conv_block(ch_in=input_channel, ch_out=dims[0])
+        self.encoder1 = CMUNeXtBlock(ch_in=dims[0], ch_out=dims[0], depth=depths[0], k=kernels[0])
+        self.encoder2 = CMUNeXtBlock(ch_in=dims[0], ch_out=dims[1], depth=depths[1], k=kernels[1])
+        self.encoder3 = CMUNeXtBlock(ch_in=dims[1], ch_out=dims[2], depth=depths[2], k=kernels[2])
+        self.encoder4 = CMUNeXtBlock(ch_in=dims[2], ch_out=dims[3], depth=depths[3], k=kernels[3])
+        # self.encoder5 = CMUNeXtBlock(ch_in=dims[3], ch_out=dims[4], depth=depths[4], k=kernels[4])
+        self.encoder5 = Grouped_multi_axis_Hadamard_Product_Attention(ch_in=dims[3], ch_out=dims[4])
+        # # Decoder
+        # self.Up5 = up_conv(ch_in=dims[4], ch_out=dims[3])
+        self.Up5 = Grouped_multi_axis_Hadamard_Product_Attention(ch_in=dims[4], ch_out=dims[3])
+        self.Att5 = Attention_block(F_g=160, F_l=160, F_int=80)
+        self.Up_conv5 = fusion_conv(ch_in=dims[3] * 2, ch_out=dims[3])
+        #
+        self.Up4 = up_conv(ch_in=dims[3], ch_out=dims[2])
+        self.Att4 = Attention_block(F_g=128, F_l=128, F_int=64)
+        self.Up_conv4 = fusion_conv(ch_in=dims[2] * 2, ch_out=dims[2])
+
+        self.Up3 = up_conv(ch_in=dims[2], ch_out=dims[1])
+        self.Att3 = Attention_block(F_g=32, F_l=32, F_int=16)
+        self.Up_conv3 = fusion_conv(ch_in=dims[1] * 2, ch_out=dims[1])
+
+        self.Up2 = up_conv(ch_in=dims[1], ch_out=dims[0])
+        self.Att2 = Attention_block(F_g=16, F_l=16, F_int=8)
+        self.Up_conv2 = fusion_conv(ch_in=dims[0] * 2, ch_out=dims[0])
+
+        self.Conv_1x1 = nn.Conv2d(dims[0], num_classes, kernel_size=1, stride=1, padding=0)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x1 = self.stem(x)
+        x1 = self.encoder1(x1)
+        x2 = self.Maxpool(x1)
+        x2 = self.encoder2(x2)
+        # print("x2的形状",x2.shape)
+        x3 = self.Maxpool(x2)
+        # print("x3数据形状:",x3.shape)
+        x3 = self.encoder3(x3)
+        x4 = self.Maxpool(x3)
+        x4 = self.encoder4(x4)
+        # x5 = self.Maxpool(x4)
+        # x5 = self.encoder5(x5)
+        x5 = self.encoder5(x4)
+
+        d5 = self.Up5(x5)
+        x4 = self.Att5(g=d5, x=x4)
+        d5 = torch.cat((x4, d5), dim=1)
+        d5 = self.Up_conv5(d5)
+
+        d4 = self.Up4(d5)
+        x3 = self.Att4(g=d4, x=x3)
+        d4 = torch.cat((x3, d4), dim=1)
+        d4 = self.Up_conv4(d4)
+
+        d3 = self.Up3(d4)
+        x2 = self.Att3(g=d3, x=x2)
+        d3 = torch.cat((x2, d3), dim=1)
+        d3 = self.Up_conv3(d3)
+
+        d2 = self.Up2(d3)
+        x1 = self.Att2(g=d2, x=x1)
+        d2 = torch.cat((x1, d2), dim=1)
+        d2 = self.Up_conv2(d2)
+
+        d1 = self.Conv_1x1(d2)
+        out = nn.Sigmoid()(d1)
+
+        return out
+
+
+def cmunext(dims=[16, 32, 128, 160, 256], depths=[1, 1, 1, 3, 1], kernels=[3, 3, 7, 7, 7]):
+    return CMUNeXt(dims=dims, depths=depths, kernels=kernels)
+
+
+def cmunext_s(dims=[8, 16, 32, 64, 128], depths=[1, 1, 1, 1, 1], kernels=[3, 3, 7, 7, 9]):
+    return CMUNeXt(dims=dims, depths=depths, kernels=kernels)
+
+
+def cmunext_l(dims=[32, 64, 128, 256, 512], depths=[1, 1, 1, 6, 3], kernels=[3, 3, 7, 7, 7]):
+    return CMUNeXt(dims=dims, depths=depths, kernels=kernels)
